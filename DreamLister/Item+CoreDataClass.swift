c1fb7642d70ef6629d1e96ceb203882e5dc5d7cd
Mago Nicolas Palacios <magonicolas@gmail.com>
2016-09-01 15:48:51 -0300

    Design Complete

--- DreamLister/Item+CoreDataClass.swift
+++ DreamLister/Item+CoreDataClass.swift
@@ -11,5 +11,10 @@ import CoreData
 
 
 public class Item: NSManagedObject {
-
+    
+    public override func awakeFromInsert() {
+        super.awakeFromInsert()
+        
+        self.created = NSDate()
+    }
 }
--- DreamLister/MainVC.swift
+++ DreamLister/MainVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainVC.Swift
 //  DreamLister
 //
 //  Created by Mago Nicolas Palacios on 9/1/16.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
