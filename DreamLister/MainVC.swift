b670f0c45e37fd4df146f431086e1aa26b9e40cf
Mago Nicolas Palacios <magonicolas@gmail.com>
2016-09-01 17:20:39 -0300

    Error on MainVC on line 39.

--- DreamLister/MainVC.swift
+++ DreamLister/MainVC.swift
@@ -7,25 +7,38 @@
 //
 
 import UIKit
+import CoreData
 
-class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFetchedResultsControllerDelegate {
     
     @IBOutlet weak var tableView: UITableView!
     
     @IBOutlet weak var segment: UISegmentedControl!
+    
+    var controller: NSFetchedResultsController<Item>!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-    //    tableView.delegate = self
-      //  tableView.dataSource = self
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
     func numberOfSections(in tableView: UITableView) -> Int {
+        if let sections = controller.sections
+        {
+            return sections.count
+        }
         return 0
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection: Int) -> Int {
+        
+        if let sections = controller.sections
+        {
+            let sectionInfo = sections[section]
+            return sectionInfo.numberOfObjects
+        }
         return 0
     }
     
@@ -33,6 +46,68 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         
         return UITableViewCell()
     }
+    
+    func attemptFetch ()
+    {
+        let fetchRequest: NSFetchRequest<Item> = Item.fetchRequest()
+        let dateSort = NSSortDescriptor(key: "created", ascending: false)
+        fetchRequest.sortDescriptors = [dateSort]
+        
+        let controller = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
+        
+        do {
+            try controller.performFetch()
+        } catch {
+            let error = error as NSError
+            print("\(error)")
+        }
+    }
+    
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>)
+    {
+        tableView.beginUpdates()
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>)
+    {
+        tableView.endUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        
+        switch(type) {
+        case.insert:
+            if let indexPath = newIndexPath {
+                tableView.insertRows(at: [indexPath], with: .fade)
+            }
+            break
+        case.delete:
+            if let indexPath = indexPath {
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+            break
+        case.update:
+            if let indexPath = indexPath
+            {
+                let cell = tableView.cellForRow(at: indexPath) as! ItemCell
+                // Update the Cell Data.
+            }
+            break
+        case.move:
+            if let indexPath = indexPath
+            {
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+            if let indexPath = newIndexPath
+            {
+                tableView.insertRows(at: [indexPath], with: .fade)
+            }
+            break
+            
+        }
+    }
+    
+    
 
 }
 
