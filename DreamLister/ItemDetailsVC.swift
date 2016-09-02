65cefc40375952c58a0d91bf69cb059138231d4e
Mago Nicolas Palacios <magonicolas@gmail.com>
2016-09-02 10:57:52 -0300

    Now You Can Edit Existing Entries and Update them.

--- DreamLister/ItemDetailsVC.swift
+++ DreamLister/ItemDetailsVC.swift
@@ -17,7 +17,7 @@ class ItemDetailsVC: UIViewController, UIPickerViewDataSource, UIPickerViewDeleg
     @IBOutlet weak var detailsField: CustomTextField!
     
     var stores = [Store]()
-    
+    var itemToEdit: Item?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -46,6 +46,11 @@ class ItemDetailsVC: UIViewController, UIPickerViewDataSource, UIPickerViewDeleg
 //        ad.saveContext()
         
         getStores()
+        
+        if itemToEdit != nil
+        {
+            loadItemData()
+        }
 
     }
     
@@ -80,7 +85,13 @@ class ItemDetailsVC: UIViewController, UIPickerViewDataSource, UIPickerViewDeleg
 
     @IBAction func savePressed(_ sender: UIButton)
     {
-        let item = Item(context: context)
+        var item: Item!
+        
+        if itemToEdit == nil {
+            item = Item(context: context)
+        } else {
+            item = itemToEdit
+        }
         
         if let title = titleField.text {
             item.title = title
@@ -98,5 +109,27 @@ class ItemDetailsVC: UIViewController, UIPickerViewDataSource, UIPickerViewDeleg
         
         ad.saveContext()
         navigationController?.popViewController(animated: true)
+    }
+    
+    func loadItemData ()
+    {
+        if let item = itemToEdit
+        {
+            titleField.text = item.title
+            priceField.text = "\(item.price)"
+            detailsField.text = item.details
+            
+            if let store = item.toStore {
+                var index = 0
+                repeat {
+                    let s = stores[index]
+                    if s.name == store.name {
+                        storePicker.selectRow(index, inComponent: 0, animated: false)
+                        break
+                    }
+                    index += 1
+                } while (index < stores.count)
+            }
+        }
     }
 }
